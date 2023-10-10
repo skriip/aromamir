d236168e63d4eca5c379f8ce9b2a70eba2898128
10210156 <10210156@B206PC11>
2023-10-10 12:33:40 +0300

    Добавьте файлы проекта.

---
+++ aromamir/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aromamir
+{
+    internal class AppData : DbContext 
+    { 
+        public static TradeEntities db = new TradeEntities ();
+        public static DbSet <User> users { get; set; }
+
+    }
+}
